327e70b54a829233a9023a521e21010d4743fc86
emesan22 <emesan@mail.emesan.click>
2023-08-02 14:25:47 +0900

    📗

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,19 @@
-export default defineNuxtConfig({
+import { NuxtConfig } from "@nuxt/types";
+
+const config: NuxtConfig = {
   // https://github.com/nuxt-themes/docus
-  extends: '@nuxt-themes/docus',
+  extends: "@nuxt-themes/docus",
 
   modules: [
     // https://github.com/nuxt-modules/plausible
-    '@nuxtjs/plausible',
+    "@nuxtjs/plausible",
     // https://github.com/nuxt/devtools
-    '@nuxt/devtools'
-  ]
-})
+    "@nuxt/devtools",
+  ],
+
+  router: {
+    base: "/docs/",
+  },
+};
+
+export default config;
